cd01ebc4778421e625bd1e023b0656dfbb0d4e21
Jacob O'Quinn <Jacob.oquinn374@gmail.com>
2021-11-13 16:59:35 -0500

    Tried to fix c

--- UCF-Contests/11:13/c.java
+++ UCF-Contests/11:13/c.java
@@ -13,15 +13,20 @@ public class c{
 		}
 	}
 
+	static int order[];
+	static Edge edges[];
+	static int numNodes;
+	static int numEdges;
+
 	public static void main(String[] args){
 		Scanner scnr = new Scanner(System.in);
 
 		int numCases = scnr.nextInt();
 		for(int caseNum = 1; caseNum <= numCases; caseNum++){
 			scnr.nextInt(); // get rid of case num
-			int numNodes = scnr.nextInt();
-			int numEdges = scnr.nextInt();
-			Edge edges[] = new Edge[numEdges];
+			numNodes = scnr.nextInt();
+			numEdges = scnr.nextInt();
+			edges = new Edge[numEdges];
 			for(int i = 0; i < numEdges; i++){
 				edges[i] = new Edge(scnr.nextInt()-1, scnr.nextInt()-1);
 			}
@@ -34,67 +39,42 @@ public class c{
 			for(Edge e : edges){
 				srcNodes[e.v] = false;
 			}
-			Queue<Integer> q = new LinkedList<Integer>();
+			order = new int[numNodes];
+			Arrays.fill(order, -1);
+			for(int i = 0; i < numNodes; i++)
+				if(srcNodes[i]) order[i] = 1;
 
-			int order[] = new int[numNodes];
-			Arrays.fill(order, 0);
 
-			for(int i = 0 ; i < numNodes; i++){
-				if(srcNodes[i]) {
-					q.add(i);
-					order[i] = 1;
-				}
+			for(int i = 0; i < numNodes; i++){
+				getOrder(i);
 			}
-
-			boolean visited[] = new boolean[numNodes];
-			Arrays.fill(visited, false);
-
-			while(!q.isEmpty()){
-				int src = q.poll();
-				if(visited[src]) continue;
-				// for(int k : order)
-				// 	System.out.print(k + " ");
-				// System.out.println();
-				for(Edge e : edges){
-					if(e.u == src){
-						// big river leading into smaller river
-						if(order[e.v] < order[src]) {
-							order[e.v] = order[src];
-							q.add(e.v);
-						}
-						// two equal rivers combine
-						else if(order[e.v] == order[src]){
-							order[e.v]++;
-							q.add(e.v);
-						}
-						// small river leading into big river
-						else{}
-					}
-				}
-				visited[src] = true;
-			}
-
-			//
-			// for(int k = 0; k < numNodes; k++){
-			// 	for(int i = 0; i < numEdges; i++){
-			// 		for(int j = 0; j < numEdges; j++){
-			// 			if(i == j) continue;
-			// 			if(edges[i].v == edges[i].v){
-			// 				int srcA = edges[i].u;
-			// 				int srcB = edges[j].u;
-			// 				int comb = (order[srcA] == order[srcB]) ? order[srcA] + 1 : Math.min(order[srcA], order[srcB]);
-			// 				order[edges[srcA].v] = Math.max(order[edges[srcA].v], comb);
-			// 			}
-			// 		}
-			// 	}
-			// }
-
-			int ans = -1;
-			for(int k : order) {
-				ans = Math.max(k, ans);
-			}
-			System.out.println(caseNum + " " + ans);
+			for(int k : order) System.out.print(k + " ");
+			System.out.println();
+			System.out.println(caseNum + " " + getOrder(numNodes-1));
 		}
 
+	}
+	static int getOrder(int cur){
+		if(order[cur] != -1) return order[cur];
+
+		int freq[] = new int[numNodes+1];
+		boolean srcVisited[] = new boolean [numNodes];
+		Arrays.fill(srcVisited, false);
+
+		int highestOrder = -1;
+		for(Edge e : edges){
+			if(e.v == cur){
+				highestOrder = Math.max(highestOrder, getOrder(e.u));
+				if(!srcVisited[e.u]) freq[getOrder(e.u)]++;
+				srcVisited[e.u] = true;
+			}
+		}
+		// check if 2+ incoming edges of had equal order
+		for(int i = 0; i < freq.length; i++){
+			if(freq[i] >= 2) highestOrder = Math.max(highestOrder, i+1);
+		}
+
+		order[cur] = highestOrder;
+		return order[cur];
 	}
 }
