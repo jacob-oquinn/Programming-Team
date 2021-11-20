c4c1cdda471bbbb516b96e08f23d01344128fcff
Jacob O'Quinn <Jacob.oquinn374@gmail.com>
2021-11-20 12:34:58 -0500

    Starting d

---
+++ UCF-Contests/21-11-13/d.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class d {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		int m = scnr.nextInt();
+		int a = 1;
+		int b = 1;
+
+		for(int len = 2; ;len++){
+
+		}
+		System.out.println(len);
+
+	}
+}
